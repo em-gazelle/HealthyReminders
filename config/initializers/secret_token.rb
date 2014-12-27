8acbd28b15036ed3a318ccc09b675f2aba232000
em-gazelle <emmaf9@gmail.com>
2014-12-26 19:06:42 -0800

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpTakeMyMeds::Application.config.secret_token = '3a439d1c4226ccca054ad680b76866d95fcaaacbf9edef45a7a5f03f6ac8a5bb1c35553fc9c007644fb15c935897ebdda384a9d47fe5bb17b6139c1e768b12e0'
